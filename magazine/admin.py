37c497c4fa51241f975f7bb7a1d56969866c0483
ChrisAchinga <achinga.chris@gmail.com>
2020-08-14 21:59:15 +0300

    custom magazine

--- magazine/admin.py
+++ magazine/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Magazine, Issue, Category, Article
 
-# Register your models here.
+# admin.site.register(Magazine)
+@admin.register(Magazine)
+class MagazineAdmin(admin.ModelAdmin):
+    list_display = ('magazine_id', 'mag_issue', 'mag_date1', 'mag_date2')
+    list_filter = ('mag_issue',)
+
+
+
+admin.site.register(Issue)
+admin.site.register(Category)
+admin.site.register(Article)
