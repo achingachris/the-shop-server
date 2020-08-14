8b141eb325df867707fa01b998b7cca48507283a
ChrisAchinga <achinga.chris@gmail.com>
2020-08-15 00:03:17 +0300

    started views

--- magazine/views.py
+++ magazine/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Issue, Magazine, Category, Article
 
 # landing page view
 def landingPage(request):
+    article_list = Article.objects.filter()
     context = {}
-    return render(request, 'magazine/index.html', context)
+    return render(request, 'magazine/index.html', {'article_list': article_list})
 
 # about page view
 def aboutPage(request):
     context = {}
     return render(request, 'magazine/about.html', context)
+
+# Article View
+
