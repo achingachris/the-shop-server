38e63ba7acc671d8b3207004d6f37fc416fd7fb7
ChrisAchinga <achinga.chris@gmail.com>
2020-08-14 22:11:12 +0300

    admin custom

--- magazine/admin.py
+++ magazine/admin.py
@@ -7,8 +7,19 @@ class MagazineAdmin(admin.ModelAdmin):
     list_display = ('magazine_id', 'mag_issue', 'mag_date1', 'mag_date2')
     list_filter = ('mag_issue',)
 
+# admin.site.register(Issue)
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('issue_id', 'issue_pub_date', 'issue_status')
+    list_filter = ('issue_id', 'issue_status',)
 
+# admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
 
-admin.site.register(Issue)
-admin.site.register(Category)
-admin.site.register(Article)
+# admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'article_status', 'category', 'article_issue')
+    list_filter = ('author', 'article_status', 'category', 'article_issue')
