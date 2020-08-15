120799d446b47fe430378b79f955eb7e04ff89ef
ChrisAchinga <achinga.chris@gmail.com>
2020-08-15 10:56:24 +0300

    js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,26 @@
-console.log('file working')
+$( document ).ready(function() {
+  $(".wrap-fade").hover(
+    function () {
+      $(this).find("img").addClass("fading");
+    },
+    function () {
+      $(this).find("img").removeClass("fading");
+    }
+  );
+});
+var windowWidth = $(window).width();
+var yourNavigation = $("#navbar");
+    yourHeader = $('.upperHeader').height();
+
+$(window).scroll(function() {
+  if(windowWidth > 768){
+    if( $(this).scrollTop() > yourHeader ) {
+      yourNavigation.addClass('sticky');
+      $('#logoSm').fadeIn("slow");
+    } else {
+      yourNavigation.removeClass('sticky');
+      $('#logoSm').fadeOut();
+    }
+  }
+
+});
