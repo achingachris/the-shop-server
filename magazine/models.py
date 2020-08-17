8bea2cc8bd8478dcd21791eb8334f8dbdebba1f5
ChrisAchinga <achinga.chris@gmail.com>
2020-08-17 14:36:53 +0300

    updated models

--- magazine/models.py
+++ magazine/models.py
@@ -24,19 +24,27 @@ class Category(models.Model):
         ('lifestyle', 'Lifestyle'),
         ('general', 'General')
     )
-    category = models.CharField('Category Name', choices=CATEGORY_LIST, default='general')
+    category = models.CharField('Category Name', choices=CATEGORY_LIST, default='general', max_length=20)
     cover_image = models.ImageField()
-    description = models.TextField()
-    category_link = models.SlugField()
+    description = models.TextField(max_length=250, null=False)
+    category_link = models.SlugField(max_length=25)
 
 # Magazine Models
 class Magazine(models.Model):
     cover_image = models.ImageField()
-    title = models.CharField()
-    description = models.CharField()
-    magazine_link = models.SlugField()
-    image1 = models.ImageField()
-    image2 = models.ImageField()
+    title = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=200, null=False)
+    publish_date = models.DateField()
+    magazine_link = models.SlugField(max_length=25, unique_for_date='publish_date')
+    image = models.ImageField()
+
+    # @property
+	# def imageURL(self):
+    #         try:
+    #             url = self.image.url
+    #         except:
+    #             url = ''
+    #         return url
 
 # Article Model
 class Article(models.Model):
@@ -46,33 +54,41 @@ class Article(models.Model):
         ('published', 'Published')
     )
 
-    cover_image = models.ImageField()
-    title = models.CharField()
-    subtitle = models.CharField()
-    description = models.CharField()
-    subheading1 = models.CharField()
+    cover_image = models.ImageField(null=False, blank=False)
+    title = models.CharField(max_length=20, null=False)
+    subtitle = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=200, null=False)
+    subheading1 = models.CharField(max_length=20, null=False)
     body1 = models.TextField()
-    subheading2 = models.CharField()
+    subheading2 = models.CharField(max_length=20, null=False)
     body2 = models.TextField()
-    subheading3 = models.CharField()
+    subheading3 = models.CharField(max_length=20, null=False)
     body3 = models.TextField()
-    body_image = models.ImageField()
-    category = models.ForeignKey(Category)
-    magazine_issue = models.ForeignKey(Magazine)
-    published = models.CharField(choices=STATUS, default='draft')
-    publish_date = models.DateField()
-    author = models.ForeignKey(Author)
-    article_link = models.SlugField()
+    body_image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    magazine_issue = models.ForeignKey(Magazine, on_delete=models.SET_NULL, null=True, blank=True)
+    published = models.CharField(choices=STATUS, default='draft', max_length=20)
+    publish_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True)
+    article_link = models.SlugField(max_length=25, unique_for_date='publish_date')
 
 # News Updates
 class News(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=20, null=False)
     image = models.ImageField()
-    description = models.CharField()
+    description = models.CharField(max_length=100, null=False)
     body = models.TextField()
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateField()
-    news_link = models.SlugField()
+    news_link = models.SlugField(max_length=25, unique_for_date='date')
+
+    # @property
+	# def imageURL(self):
+	# 	try:
+	# 		url = self.image.url
+	# 	except:
+	# 		url = ''
+	# 	return url
 
 # subscriptions
 class Subscription(models.Model):
