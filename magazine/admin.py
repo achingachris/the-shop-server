286dfba16061036d44e6f87ef7df6f2fa7d2533c
ChrisAchinga <achinga.chris@gmail.com>
2020-08-17 14:50:29 +0300

    add to admin

--- magazine/admin.py
+++ magazine/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Magazine)
+admin.site.register(Article)
+admin.site.register(News)
--- magazine/models.py
+++ magazine/models.py
@@ -25,18 +25,18 @@ class Category(models.Model):
         ('general', 'General')
     )
     category = models.CharField('Category Name', choices=CATEGORY_LIST, default='general', max_length=20)
-    cover_image = models.ImageField()
+    cover_image = models.ImageField(upload_to='uploads/')
     description = models.TextField(max_length=250, null=False)
     category_link = models.SlugField(max_length=25)
 
 # Magazine Models
 class Magazine(models.Model):
-    cover_image = models.ImageField()
+    cover_image = models.ImageField(upload_to='uploads/')
     title = models.CharField(max_length=100, null=False)
     description = models.CharField(max_length=200, null=False)
     publish_date = models.DateField()
     magazine_link = models.SlugField(max_length=25, unique_for_date='publish_date')
-    image = models.ImageField()
+    image = models.ImageField(upload_to='uploads/')
 
     # @property
 	# def imageURL(self):
@@ -54,7 +54,7 @@ class Article(models.Model):
         ('published', 'Published')
     )
 
-    cover_image = models.ImageField(null=False, blank=False)
+    cover_image = models.ImageField(upload_to='uploads/')
     title = models.CharField(max_length=20, null=False)
     subtitle = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=200, null=False)
@@ -64,7 +64,7 @@ class Article(models.Model):
     body2 = models.TextField()
     subheading3 = models.CharField(max_length=20, null=False)
     body3 = models.TextField()
-    body_image = models.ImageField(null=True, blank=True)
+    body_image = models.ImageField(upload_to='uploads/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     magazine_issue = models.ForeignKey(Magazine, on_delete=models.SET_NULL, null=True, blank=True)
     published = models.CharField(choices=STATUS, default='draft', max_length=20)
@@ -75,7 +75,7 @@ class Article(models.Model):
 # News Updates
 class News(models.Model):
     title = models.CharField(max_length=20, null=False)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='uploads/')
     description = models.CharField(max_length=100, null=False)
     body = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
