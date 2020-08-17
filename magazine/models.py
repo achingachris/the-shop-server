c4900b13caef980fa947e4b2bd77883d839a7c9c
ChrisAchinga <achinga.chris@gmail.com>
2020-08-17 13:49:21 +0300

    added

--- magazine/models.py
+++ magazine/models.py
@@ -5,11 +5,29 @@ from django.contrib.auth.models import User
 
 # Author Models
 class Author(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 # Categories Models
 class Category(models.Model):
-    pass
+    # list of required categories required
+    CATEGORY_LIST = (
+        ('beauty', 'Beauty'),
+        ('trends', 'Travel'),
+        ('travel', 'Travel'),
+        ('fashion', 'Fashion'),
+        ('culture', 'Culture'),
+        ('food', 'Food'),
+        ('entrepreneur', 'Entrepreneur'),
+        ('fashion-pictorial', 'Fashion Pictorial'),
+        ('health', 'Health'),
+        ('men', 'Mens Corner'),
+        ('lifestyle', 'Lifestyle'),
+        ('general', 'General')
+    )
+    category = models.CharField('Category Name', choices=CATEGORY_LIST, default='general')
+    cover_image = models.ImageField()
+    description = models.TextField()
+    category_link = models.SlugField()
 
 # Article Model
 class Article(models.Model):
