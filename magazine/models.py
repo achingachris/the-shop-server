8c91d6223cfbc3eb82a99e2cc5a283608f6020cd
ChrisAchinga <achinga.chris@gmail.com>
2020-08-17 13:33:59 +0300

    init models

--- magazine/models.py
+++ magazine/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Author Models
+class Author(models.Model):
+    pass
+
+# Categories Models
+class Category(models.Model):
+    pass
+
+# Article Model
+class Article(models.Model):
+    pass
+
+# Magazine Models
+class Magazine(models.Model):
+    pass
+
+# News Updates
+class News(models.Model):
+    pass
+
+# subscriptions
+class Subscription(models.Model):
+    pass
+
+# image gallery
+class ImageGallery(models.Model):
+    pass
+
+# video gallery
+class VideoGallery(models.Model):
+    pass
