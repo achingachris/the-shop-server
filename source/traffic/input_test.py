a85db3108c6f56acb6e3fcf67b6794d57fe0b791
ChrisAchinga <achinga.chris@gmail.com>
2021-02-25 22:32:18 +0300

    test all inputs

--- source/traffic/input_test.py
+++ source/traffic/input_test.py
@@ -1,11 +1,43 @@
 # testing input from a text file
-# f = open("test.txt", "r")
-# print(f.read())
-# print(f.readline())
-# f.close()
+f = open("test.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
 
-filename = "test.txt"
+f = open("a.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
 
-with open(filename, 'r') as filehandle:
-    for line in filehandle:
-        print(line)
+f = open("b.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
+
+f = open("c.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
+
+f = open("d.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
+
+f = open("e.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
+
+f = open("f.txt", "r")
+print(f.read())
+print(f.readline())
+f.close()
+
+print('Successfull Read and Print')
+
+# filename = "test.txt"
+
+# with open(filename, 'r') as filehandle:
+#     for line in filehandle:
+#         print(line)
