4fef37ca5782e9f358264aa1ee1a0600f807b1b6
ChrisAchinga <achinga.chris@gmail.com>
2021-06-20 15:47:12 +0300

    clear

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b67c699f331cfa733cef1040802b7c4e'),
-    },
-  },
-});
--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '85442bbe-9b03-4b65-993c-d634964ef52f'
-};
