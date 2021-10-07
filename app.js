1750c62bc9c8c8ead99c234a5f9a064db6cb30fb
chrisachinga <achinga.chris@gmail.com>
2021-10-07 21:59:36 +0300

    app entry point

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
 
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('API running Successfully')
+  res.send('API is running....')
 })
 
 app.get('/api/products', (req, res) => {
@@ -23,5 +27,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
